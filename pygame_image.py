dce841587705d7330f2b757ff128fc63208c2919
eno <c0a2202682@edu.teu.ac.jp>
2023-04-18 18:15:49 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -6,23 +6,27 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)]*2
     kf_img = pg.image.load("ex01/fig/3.png")
     kf_img = pg.transform.flip(kf_img, True, False)
-    kf_imgs = [pg.transform.rotozoom(kf_img, 10, 1.0), kf_img]
+    kf_imgs = [kf_img, pg.transform.rotozoom(kf_img, 10, 1.0)]
 
     tmr = 0
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT:
+                return
 
         tmr += 1
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [1600 - tmr, 0])
-        screen.blit(kf_imgs[tmr%2], [300, 200])
-
+        x = tmr%3200
+        for i in range(4):
+            screen.blit(bg_imgs[i], [1600 * i - x, 0])
+        screen.blit(kf_imgs[tmr % 100//50], [300, 200])
+        
+        print(tmr)
         pg.display.update()
-        clock.tick(100)
+        clock.tick(800)
 
 
 if __name__ == "__main__":
